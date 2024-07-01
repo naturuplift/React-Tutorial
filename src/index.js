9246f7630ef4ab3aa1f120d503160acee2ad17d7
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-07-01 11:19:08 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function Greeting() {
+  return <h1>My First Component</h1>;
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Greeting>
+  </Greeting>
 );
 
 // If you want to start measuring performance in your app, pass a function
