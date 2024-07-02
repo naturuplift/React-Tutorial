31b39b22d452cd349cc3087395fc791a0942c181
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-07-02 08:03:54 -0300

    Update Booklist.js

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,30 +1,45 @@
 import React from 'react'
 import './Booklist.css'
 
-const author = 'Freida McFadden'
-const title = 'The Housemaid Is Watching'
-const img = './images/book-1.jpg'
+const firstBook = {
+  author: 'Freida McFadden',
+  title: 'The Housemaid Is Watching',
+  img: './images/book-1.jpg',
+}
+const secondBook = {
+  author: 'James Clear',
+  title: 'Atomic Habits',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+}
 
 const Booklist = () => {
   return (
     <section className='booklist'>
-      <Book author={author} title={title} img={img} />
-      <Book author={author} title={title} img={img} />
-      <Book title='random title' number={22} />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   )
 }
 
 const Book = (props) => {
-    console.log(props)
+  console.log(props)
+  const { img, title, author } = props
   return (
     <article className='book'>
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
-      {/* <p>{props.job}</p>
-      <p>{props.title}</p>
-      <p>{props.number}</p> */}
+      {/* <p>{job}</p>
+      <p>{title}</p>
+      <p>{number}</p> */}
     </article>
   )
 }
