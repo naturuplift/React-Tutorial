98e4f03be6f90295e38fbc3b14c976ad8042c18b
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-07-02 06:54:47 -0300

    Update Booklist.js

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -15,7 +15,7 @@ function Booklist() {
 
 const Book = () => {
   return (
-    <article>
+    <article className='book'>
       <Image />
       <Title />
       <Author />
@@ -26,17 +26,23 @@ const Book = () => {
 const Image = () => (
   <img src='./images/book-1.jpg' alt='The Housemaid Is Watching' />
 )
+// const Title = () => {
+//     const inlineHeadingStyles = {
+//       color: '#1e12d7',
+//       fontSize: '1.5rem',
+//       marginTop: '0.9rem',
+//       marginBottom: '0.3rem'
+//     }
+//   return <h2 style={inlineHeadingStyles}>The Housemaid Is Watching</h2>
+// }
 const Title = () => {
-    const inlineHeadingStyles = {
-      color: '#1e12d7',
-      fontSize: '1.5rem',
-      marginTop: '0.9rem',
-      marginBottom: '0.3rem'
-    }
-  return <h2 style={inlineHeadingStyles}>The Housemaid Is Watching</h2>
+    return <h2>The Housemaid Is Watching</h2>
 }
+// const Author = () => (
+// <h4 style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.5rem'}} >Freida McFadden</h4>
+// )
 const Author = () => (
-<h4 style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.5rem'}} >Freida McFadden</h4>
+  <h4>Freida McFadden</h4>
 )
 
 export default Booklist
