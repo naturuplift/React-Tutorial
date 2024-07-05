39709430ef5beb0aeae5d6e88c924172eb6f63b1
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-07-04 23:58:22 -0300

    Update Booklist.js

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -6,11 +6,13 @@ const books = [
     author: 'Freida McFadden',
     title: 'The Housemaid Is Watching',
     img: './images/book-1.jpg',
+    id: 1,
   },
   {
     author: 'James Clear',
     title: 'Atomic Habits',
     img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+    id: 2,
   },
 ]
 
@@ -18,8 +20,8 @@ const Booklist = () => {
   return (
     <section className='booklist'>
       {books.map((book) => {
-        const { img, title, author } = book
-        return <Book img={img} title={title} author={author} />
+        const { img, title, author, id } = book
+        return <Book img={img} title={title} author={author} key={id}/>
       })}
     </section>
   )
