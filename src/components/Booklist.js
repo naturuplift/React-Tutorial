0155d23fafbaae2a80959321dd0f2ee6adf93352
Arnaldo Sepulveda <arnaldosepulveda@gmail.com>
2024-07-06 00:16:51 -0300

    Update Booklist.js

--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -27,6 +27,29 @@ const Booklist = () => {
   )
 }
 
+const EventExamples = () => {
+  const handleFormInput = () => {
+    console.log('handle form input')
+  }
+  const handleButtonClick = () => {
+    alert('handle button click')
+  }
+  return (
+    <section>
+      <form>
+        <h2>Typical Form</h2>
+        <input
+          type='text'
+          name='example'
+          onChange={handleFormInput}
+          style={{ margin: '1rem 0' }}
+        />
+      </form>
+      <button onClick={handleButtonClick}>click me</button>
+    </section>
+  )
+}
+
 const Book = (props) => {
   const { img, title, author } = props
   console.log(props)
@@ -36,17 +59,6 @@ const Book = (props) => {
       <h2>{title}</h2>
       <h4>{author}</h4>
     </article>
-  )
-}
-
-const EventExamples = () => {
-  const handleButtonClick = () => {
-    alert('handle button click')
-  }
-  return (
-    <section>
-      <button onClick={handleButtonClick}>click me</button>
-    </section>
   )
 }
 
